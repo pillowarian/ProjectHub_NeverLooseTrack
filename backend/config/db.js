5794298fb6205611b1aab360fd7c2e30ebbe78ab
MD Hasin Anjum <127542579+pillowarian@users.noreply.github.com>
2025-12-05 23:15:12 +0600

    Adding backend files

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'project_feed_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 10000 // 10 seconds
+});
+
+module.exports = pool;
